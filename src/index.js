f51f6df6502b0667eefb9c303ef5b7afde6c02c0
Dito Cahya Pratama <ditocahyapratama717@gmail.com>
2020-07-11 21:43:06 +0700

    Create Register Form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
 	<React.StrictMode>
