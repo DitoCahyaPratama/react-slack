67768c88288d54c5c509b3e97bc9d47c375aae66
Dito Cahya Pratama <ditocahyapratama717@gmail.com>
2020-07-13 14:07:51 +0700

    Form Validation for Register Form

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -11,13 +11,51 @@ class Register extends Component {
             username: '',
             email: '',
             password: '',
-            passwordConfirmation: '',
+			passwordConfirmation: '',
+			errors: []
         };
-    }
+	}
+	
+	isFormValid = () => {
+		let errors = []
+		let error
+	
+		if(this.isFormEmpty(this.state)){
+			error = { message: 'Fill in all fields' }
+			this.setState({ errors: errors.concat(error) })
+			return false
+		}else if(this.isPasswordValid(this.state)){
+			error = { message: 'Password is invalid' }
+			this.setState({ errors: errors.concat(error) })
+			return false
+		}else{
+			return true
+		}
+	}
+
+	isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+		return !username.length || !email.length || !password.length || !passwordConfirmation.length
+	}
+
+	isPasswordValid = ({ password, passwordConfirmation }) => {
+		if(password.length < 6 || passwordConfirmation.length < 6){
+			return false
+		}else if(password !== passwordConfirmation){
+			return false	
+		}else{
+			return true
+		}
+	}
+
+	displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>)
+
     handleChange = e =>{
         this.setState({ [e.target.name]: e.target.value })
 	}
 	handleSubmit = e => {
+		if(this.isFormValid()){
+
+		}
 		e.preventDefault()
 		firebase
 		.auth()
@@ -30,12 +68,12 @@ class Register extends Component {
 		})
 	}
 	render() {
-        const {username, email, password, passwordConfirmation} = this.state;
+        const {username, email, password, passwordConfirmation, errors} = this.state;
 		return (
 			<Grid textAlign="center" verticalAlign="middle" className="app">
 				<Grid.Column style={{ maxWidth: 450 }}>
 					<Header as="h2" icon color="orange" textAlign="center">
-						<Icon name="puzzle piece" color="orange" />
+						<Icon na me="puzzle piece" color="orange" />
 						Register for DevChat
 					</Header>
 					<Form onSubmit={this.handleSubmit}  size="large">
@@ -85,6 +123,12 @@ class Register extends Component {
 							</Button>
 						</Segment>
 					</Form>
+					{errors.length > 0 && (
+						<Message error>
+							<h3>Error</h3>
+							{this.displayErrors(errors)}
+						</Message>
+					)}
 					<Message>
 						Already a user ? <Link to="/login">Login</Link>
 					</Message>
